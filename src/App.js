23edbb2b74757457de437c197dbc714bb829dc30
weijunh <515919409@qq.com>
2020-07-16 10:54:21 +0800

    wjh-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import Search from './components/search/search.jsx'
-import Main from './components/main/main.jsx'
+
 function App () {
   return (
     <div className="App">
       <Search />
-      <Main />
+
     </div>
   );
 }
